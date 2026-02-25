f6bb4c10533cdb44c8ed9d43d8f27cb86765ef99
leoransh <async.mohitt@gmail.com>
2026-02-25 15:04:54 +0530

    refactor: simplify user existence check in registration route

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -9,19 +9,20 @@ authRouter.post('/register', async (req,res)=>{
     const {username, email , password , profileImage ,bio} =  req.body
 
 
-    const isUserExistByUsername  =  await userModel.findOne({username})
+    // 
+    
 
+    const isUserAlreadyExist = await userModel.findOne({
+        $or:[
+            {username},
+            {email}
+        ]
+    })
 
-     if(isUserExistByUsername){
+    if(isUserAlreadyExist) {
         return res.status(409).json({
-            message:"User Already exist with this Username"
-        })
-     }
-     const isUserExistByEmail =  await userModel.findOne({email})
-
-     if(isUserExistByEmail) {
-        return res.status(409).json({
-            message:"User Already exist with the Email"
-        })
-     }
+            message:"User already Exists"
+            }
+        )
+    }
 })
