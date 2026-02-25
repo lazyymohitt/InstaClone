a448a82cf8f22d39eb1063c00b154f0f87b121ee
leoransh <async.mohitt@gmail.com>
2026-02-25 15:10:37 +0530

    fix: correct user existence check message in registration route

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,28 +1,22 @@
-const express =  require('express')
-const userModel = require('../models/user.model')
+const express = require("express");
+const userModel = require("../models/user.model");
 
-const authRouter =  express.Router()
+const authRouter = express.Router();
 
-authRouter.post('/register', async (req,res)=>{
+authRouter.post("/register", async (req, res) => {
+  const { username, email, password, profileImage, bio } = req.body;
 
+  //
 
-    const {username, email , password , profileImage ,bio} =  req.body
-
-
-    // 
-    
-
-    const isUserAlreadyExist = await userModel.findOne({
-        $or:[
-            {username},
-            {email}
-        ]
-    })
-
-    if(isUserAlreadyExist) {
-        return res.status(409).json({
-            message:"User already Exists"
-            }
-        )
-    }
-})
+  const isUserAlreadyExist = await userModel.findOne({
+    $or: [{ username }, { email }],
+  });
+  if (isUserAlreadyExist) {
+    return res.status(409).json({
+      message:
+        "User already Exists" + iUserAlreadyExist.email === email
+          ? "Email Is already Used"
+          : "Username Is already use:",
+    });
+  }
+});
