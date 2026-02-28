d241cf87ebe9e18eeaa80c460c51c6d8b4cf6340
leoransh <async.mohitt@gmail.com>
2026-03-01 01:35:37 +0530

    feat: refactor authentication logic by moving register and login handlers to auth.controller

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,102 @@
+const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+
+async function registerController(req, res) {
+  const { username, email, password, profileImage, bio } = req.body;
+
+  //
+
+  const isUserAlreadyExist = await userModel.findOne({
+    $or: [{ username }, { email }],
+  });
+  if (isUserAlreadyExist) {
+    return res.status(409).json({
+      message:
+        isUserAlreadyExist.email === email
+          ? "Email Is already Used"
+          : "Username Is already use:",
+    });
+  }
+
+  const hash = crypto.createHash("sha256").update(password).digest("hex");
+
+  //   this code is used to create the user in out dataBase
+  const user = await userModel.create({
+    email,
+    username,
+    bio,
+    profileImage,
+    password: hash,
+  });
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" },
+  );
+
+  res.cookie("token", token);
+
+  res.status(201).json({
+    message: "User Is SuccesFully Created",
+
+    user: {
+      username: user.username,
+      profileImage: user.profileImage,
+      email: user.email,
+      bio: user.bio,
+    },
+  });
+}
+
+async function loginController (req, res)  {
+  const { email, username, password } = req.body;
+
+  const user = await userModel.findOne({
+    $or: [{ username: username }, { email: email }],
+  });
+
+  if (!user) {
+    return res.status(404).json({
+      message: "Username and passsword are InCorrect",
+    });
+  }
+
+  const hash = crypto.createHash("sha256").update(password).digest("hex");
+
+  const isPasswordValid = hash == user.password;
+
+  if (!isPasswordValid) {
+    return res.status(401).json({
+      message: "You've Entered a Wrong Password",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" },
+  );
+
+  res.cookie("token", token);
+
+  res.status(200).json({
+    message: "user loggedIn Succesfully",
+    user: {
+      username: user.username,
+      email: user.email,
+      bio: user.bio,
+      profileImage: user.profileImage,
+    },
+  });
+};
+
+module.exports = {
+    registerController,
+    loginController
+}
