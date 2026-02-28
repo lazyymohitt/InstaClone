83c011731cb968d5faa107582b7ab019bcf272cc
leoransh <async.mohitt@gmail.com>
2026-03-01 03:05:04 +0530

    feat: add post model with schema for imageUrl, caption, and user reference

---
+++ src/models/post.model.js
@@ -0,0 +1,24 @@
+const mongoose  = require("mongoose")
+
+
+
+const postSchema =  new mongoose.Schema({
+    imageUrl:{
+        type:String,
+        required:[true,"Image Url Is Required"]
+    },
+    caption:{
+        type:String,
+        default:""
+    },
+    user:{
+        ref:"user",
+        type:mongoose.Schema.Types.ObjectId,
+        required:[true, "userId Is required"]
+    }
+})
+
+const postModel = mongoose.model("posts", postSchema)
+
+
+module.exports = postModel
