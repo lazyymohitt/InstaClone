ded038cf64d577d4dec227b9cef50667478f6fd8
leoransh <async.mohitt@gmail.com>
2026-04-24 22:06:30 +0530

    feat: add validation to prevent following oneself in followUserController

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,12 +16,20 @@ async  function followUserController(req,res){
       followee: followeeUsername,
     });
 
+    if(followeeUsername == followerUsername){
+        res.status(401).json({
+            message:"Youre already following the User"
+        })
+    }
+
 
 
     res.status(201).json({
       message: `You are now following ${followeeUsername}`,
       follow: followRecord,
     })
+
+
 }
 
 module.exports = {
