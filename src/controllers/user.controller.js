dedc95c302fe9cdc0ad7cdc2203b7249249c9970
leoransh <async.mohitt@gmail.com>
2026-04-24 22:22:20 +0530

    refactor: clean up followUserController for better readability and maintainability

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,37 +1,28 @@
-const followModel =  require("../models/follow.model")
+const followModel = require("../models/follow.model");
 
+async function followUserController(req, res) {
+  // This Is The LoggedIn USER
+  const followerUsername = req.user.username;
+  const followeeUsername = req.params.username;
 
-async  function followUserController(req,res){
+  const followRecord = await followModel.create({
+    follower: followerUsername,
+    followee: followeeUsername,
+  });
+  // Now Fixing , If the User want to follow Himself
 
-
-
-    // This Is The LoggedIn USER
-    const followerUsername =  req.user.username
-    const followeeUsername =  req.params.username
-     
-
-   
-    const followRecord = await followModel.create({
-      follower: followerUsername,
-      followee: followeeUsername,
+  if (followeeUsername == followerUsername) {
+    return res.status(400).json({
+      message: "You Cant follow YourSelf ",
     });
+  }
 
-    if(followeeUsername == followerUsername){
-        res.status(401).json({
-            message:"Youre already following the User"
-        })
-    }
-
-
-
-    res.status(201).json({
-      message: `You are now following ${followeeUsername}`,
-      follow: followRecord,
-    })
-
-
+  res.status(201).json({
+    message: `You are now following ${followeeUsername}`,
+    follow: followRecord,
+  });
 }
 
 module.exports = {
-    followUserController
-}
+  followUserController,
+};
