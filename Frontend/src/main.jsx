a6362f8efd170ed685e986d16ba573af48545767
leoransh <async.mohitt@gmail.com>
2026-06-02 11:55:24 +0530

    feat: add PostContext and PostContextProvider for managing post-related state

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -8,6 +8,7 @@ import "./style.scss";
 import Register from "./Features/Auth/Pages/Register.jsx";
 
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { PostContext, PostContextProvider } from "./Features/posts/post.context.jsx";
 
 const router = createBrowserRouter([
   {
@@ -26,7 +27,9 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
+    <PostContextProvider>
       <RouterProvider router={router} />
+    </PostContextProvider>
     </AuthProvider>
   </StrictMode>,
 );
