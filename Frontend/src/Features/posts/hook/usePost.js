8f562f0bdc355aeb72514da26762b7c0971e8aa1
leoransh <async.mohitt@gmail.com>
2026-06-02 12:36:14 +0530

    feat: create usePost hook for managing post state and fetching feed data

---
+++ Frontend/src/Features/posts/hook/usePost.js
@@ -0,0 +1,22 @@
+import { getFeed } from "../services/post.api";
+
+
+import { useContext } from "react";
+import {PostContext} from "../post.Context"
+
+
+export const usePost  = ()=>{
+
+    const context = useContext(PostContext)
+
+    const{loading,setLoading,setFeed ,feed , post, setPost} = context
+
+    const handleGetFeed = async()=>{
+        setLoading(true)
+        const data = await getFeed()
+        setFeed(data.posts)
+        setLoading(false)
+    }
+    return(loading, feed , post , handleGetFeed)
+
+}
