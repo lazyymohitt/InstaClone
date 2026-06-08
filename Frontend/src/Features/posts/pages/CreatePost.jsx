710f03dc9195a26c74951290515b70b45987dcf6
lazyymohitt <async.mohitt@gmail.com>
2026-06-08 19:24:55 +0530

    feat: create CreatePost component with form layout and styling

--- Frontend/src/Features/posts/pages/CreatePost.jsx
+++ Frontend/src/Features/posts/pages/CreatePost.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import "../style/createPost.scss"
 
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
+    <div className='create-post-page'>
+        <h1>
+            Create Post
+        </h1>
+
+        <div className="form-container">
+            <form>
+            <label id='post-image-label' htmlFor="postImage">Choose Image</label>
+                <input hidden type="file" name='postImage' id='postImage' />
+                <input type="text" id='caption' name='caption' placeholder='Enter Caption' />
+                <butto className ="button ">Create Post</butto>
+            </form>
+        </div>
+    </div>
   )
 }
 
